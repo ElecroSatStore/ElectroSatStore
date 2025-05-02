c4348d98935b2045112b55bdb7bce8fcc25ea0a2
ElecroSatStore <benoitmessi26@gmail.com>
2025-05-03 01:11:50 +0200

    Update Footer.jsx
    ok

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,17 +9,18 @@ export default function Footer() {
             <CopyRight width={12} height={12} color={'white'}/>
             <p className="mb-0 text-sm text-white font-medium">2025, Yahia Slimani</p>
         </div>
+      <br>
           <div className="flex items-center gap-4">
         <a 
-            href="https://www.facebook.com/toncompte" 
+            href="https://www.instagram.com/info_elecro_sat/?igsh=djl4cmN4djAyaHRz" 
             target="_blank" 
             rel="noopener noreferrer"
             className="text-white text-sm font-medium hover:underline"
         >
-            Facebook
+            Instagram
         </a>
         <a 
-            href="https://www.tiktok.com/@toncompte" 
+            href="https://www.tiktok.com/@raouf_sat?_t=ZN-8vzvpi56Smc&_r=1" 
             target="_blank" 
             rel="noopener noreferrer"
             className="text-white text-sm font-medium hover:underline"
@@ -27,9 +28,6 @@ export default function Footer() {
             TikTok
         </a>
     </div>
-        <div>
-
-        </div>
     </footer>
   )
 }
