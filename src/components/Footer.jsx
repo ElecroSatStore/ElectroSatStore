55b226abca7452d895a9003b5289da87a439d345
ElecroSatStore <benoitmessi26@gmail.com>
2025-05-03 00:47:44 +0200

    Update Footer.jsx
    ...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import { CopyRight } from "../../svg";
 export default function Footer() {
   return (
     <footer className="mt-10 bg-gray-900 flex justify-between px-10 py-5">
-        <h1 className="mb-0 font-bold text-white text-2xl">IPDEMP</h1>
+        <h1 className="mb-0 font-bold text-white text-2xl">SatStore</h1>
         <div className="flex items-center gap-1">
-            <p className="mb-0 text-sm text-white font-medium">Tous les Droits</p>
+            <p className="mb-0 text-sm text-white font-medium">Tous droits réservés</p>
             <CopyRight width={12} height={12} color={'white'}/>
-            <p className="mb-0 text-sm text-white font-medium">Developé par Yahia Slimani</p>
+            <p className="mb-0 text-sm text-white font-medium">2025, Yahia Slimani</p>
         </div>
         <div>
 
