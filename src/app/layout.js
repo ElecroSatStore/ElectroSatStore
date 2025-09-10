66e3125888c8a3477ccf0d3a175aed3b0d286b26
ElecroSatStore <benoitmessi26@gmail.com>
2025-09-10 22:29:58 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,19 +23,35 @@ export default function RootLayout({ children }) {
     <head>
 <script>
 !function (w, d, t) {
-  w.TiktokAnalyticsObject=t;
-  var ttq=w[t]=w[t][];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
-  ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
-  for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
-var e=ttq._i[t][],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};
-                                                                                                                  n=document.createElement("script");
-                                                                                                                  n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+  w.TiktokAnalyticsObject = t;
+  var ttq = w[t] = w[t] || [];
+  ttq.methods = ["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+  ttq.setAndDefer = function (obj, method) {
+    obj[method] = function () { obj.push([method].concat([].slice.call(arguments, 0))); };
+  };
+  for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]);
 
+  ttq.instance = function (id) {
+    ttq._i = ttq._i || {};
+    var inst = ttq._i[id] || [];
+    for (var n = 0; n < ttq.methods.length; n++) ttq.setAndDefer(inst, ttq.methods[n]);
+    return inst;
+  };
 
-  ttq.load('D2F2GIJC77U9PLHEK440');
-  ttq.page();
+  ttq.load = function (id, opts) {
+    var src = "https://analytics.tiktok.com/i18n/pixel/events.js";
+    ttq._i = ttq._i || {}; ttq._i[id] = ttq._i[id] || []; ttq._i[id]._u = src;
+    ttq._t = ttq._t || {}; ttq._t[id] = +new Date;
+    ttq._o = ttq._o || {}; ttq._o[id] = opts || {};
+    var s = d.createElement("script"); s.type = "text/javascript"; s.async = true; s.src = src + "?sdkid=" + id + "&lib=" + t;
+    var x = d.getElementsByTagName("script")[0]; x.parentNode.insertBefore(s, x);
+  };
+
+  ttq.load('D2F2GIJC77U9PLHEK440');
+  ttq.page();
 }(window, document, 'ttq');
 </script>
+
     </head>
       <body
         className={`${GeistSans.className}`}
